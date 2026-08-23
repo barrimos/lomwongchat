33e60f9ac86f5e82949881e15def40a1c4c3cec6
f <prapas.hkr@gmail.com>
2026-08-23 13:54:20 +0700

    Rollback fixing cors vercel.json

--- backend/api/index.js
+++ backend/api/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
     
     // IF ORIGIN IS UNDEFINED: Force it to assign your real frontend URL
     if (!origin) {
-      return callback(null, true)
+      return callback(null, 'https://lomwongchat.vercel.app')
     }
     
     if (allowedOrigins.includes(origin)) {
